a260a0e6791d14ae10b886000a6b09777d5d3cb2
Owen Radcliffe <62451536+radcliffeowe@users.noreply.github.com>
2022-02-22 22:06:53 -0500

    Scan.c, works with multiple threads
    Works with multiple manually created threads, need to implement arbitrary thread handling.

--- scan.c
+++ scan.c
@@ -1,112 +1,210 @@
 #include <assert.h>
+
 #include <stdio.h>
+
 #include <math.h>
+
 #include <pthread.h>
+
 #include <semaphore.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
+ 
 
 #define MAX_LINE_SIZE 256
 
-int chunk_size = 0;
-int neighbor_distance = 1;
 int* input;
-int* copy;
+
+int* seq;
+
+sem_t sem;
+
+int workl;
 
 void read_input_vector(const char* filename, int n, int* array)
+
 {
-  	FILE *fp;
-  	char *line = malloc(MAX_LINE_SIZE+1);
-  	size_t len = MAX_LINE_SIZE;
-  	ssize_t read;
 
-  	fp = strcmp(filename, "-") ? fopen(filename, "r") : stdin;
+           FILE *fp;
 
-  	assert(fp != NULL && line != NULL);
+           char *line = malloc(MAX_LINE_SIZE+1);
 
-  	int index = 0;
+           size_t len = MAX_LINE_SIZE;
 
-  	while ((read = getline(&line, &len, fp)) != -1)
-  	{
-    	array[index] = atoi(line);
-    	index++;
-  	}
+           ssize_t read;
 
-  	free(line);
-  	fclose(fp);
+ 
+
+           fp = strcmp(filename, "-") ? fopen(filename, "r") : stdin;
+
+ 
+
+           assert(fp != NULL && line != NULL);
+
+ 
+
+           int index = 0;
+
+ 
+
+           while ((read = getline(&line, &len, fp)) != -1)
+
+           {
+
+           array[index] = atoi(line);
+
+           index++;
+
+           }
+
+ 
+
+           free(line);
+
+           fclose(fp);
+
 }
+
+ 
 
 typedef struct __step_arg_t {
-	int dst; // distance between "neighbors"
-	int beg; // first operation
-	int end; // final operation
+
+           //int dst; // distance between "neighbors"
+
+           int beg; // first operation
+
+           int end; // final operation
+           
+           int step; //d
+
 } step_arg_t;
 
-void* sum_neighbors(void* step_args)
+ 
+
+void* step_sum(void* step_args)
 {
-	step_arg_t* m = (step_arg_t*) step_args;
-    // add up values [neighbor_distance] to the left of each value, from start
-    for (int op = m->beg; op <= m->end; op++) {
-    	copy[op] += input[op - m->dst]; // data sharing w/ original arr, need to use a copy arr
-    }
-    return NULL;
+	//need to pass in start index, end index, cond var thread is waiting for	
+	
+	step_arg_t* arg = (step_arg_t*)step_args;
+	int k = arg->beg;
+	int d = arg->step;
+	//printf("%d %d\n", arg->beg, arg->step);fflush(stdout);
+	while(k>=arg->end) //index of input array, by starting at highest index, we avoid adding array value that has already been updated
+	{
+		if(k >= (1u << d))
+		{
+			if(d%2 == 0)
+				seq[k] = input[(k) - (1u << d)] + input[k]; //critical section
+			else
+				input[k] = seq[(k) - (1u << d)] + seq[k];
+		}
+		else
+		{
+			if(d%2 == 0)
+				seq[k] = input[k]; //critical section
+			else
+				input[k] = seq[k];			
+		}
+		k--;
+	}  
+	return 0;
 }
+
+ 
 
 int main(int argc, char* argv[])
+
 {
+
     char* filename = argv[1];
-  	int n = atoi(argv[2]);
-  	int num_threads = atoi(argv[3]);
 
-  	if (n < 2 || num_threads < 1)
-  	  	exit(EXIT_FAILURE);
+           int n = atoi(argv[2]);
 
-  	int *input= malloc(sizeof(int) * n);
-  	read_input_vector(filename, n, input);
-  	
-  	int ops = n - 1; // total number of operations (at the start)
-  	int step = 0; // current step
-  	while (ops > 0) { // while loop for each step
-  		int ops_per_thread = ops / num_threads;
-  		int extra_ops = ops_per_thread % num_threads; // TODO: divide this evenly between threads
-  		// make one copy of input arr for each step (avoid data sharing)
-  		copy = (int*) malloc(sizeof(int) * n);
-  		for (int i = 0; i < n; i++) {
-  			copy[i] = input[i];
-  		}
-  		// initialize threads for each step
-  		pthread_t* pthreads[num_threads];
-  		
-  		for (int thread = 0; thread < num_threads; thread++) { // for each thread
-  		
-  			pthread_t tid; 			// initialize this thread
-  			pthreads[thread] = &tid;	// add to list of threads
-  			// assemble arguments for this thread
-  			step_arg_t args;
-  			args.dst = 1u << step; // 2^n 
-  			args.beg = ops_per_thread * thread + args.dst;
-  			args.end = ops_per_thread * (thread + 1) + args.dst - 1;
-  			if (thread == num_threads - 1)	// add all extra operations to final thread TODO: change
-  				args.end += extra_ops;
-  			// create thread
-  			pthread_create(&tid, NULL, sum_neighbors, (void*) &args);
-  		}
-  		// join threads after each step
-  		for (int thread = 0; thread < num_threads; thread++) {
-  			pthread_join((pthread_t) pthreads[thread], NULL);
-  		}
-  		// TODO: fill input arr with copy arr data
-  		for (int i = 0; i < n; i++) {
-  			input[i] = copy[i];
-  		}
-  		
-  		ops -= 1u << step; // ops -= 2^step# after every step
-  		step++;
-  	}
-  
-  	for (int i=0; i < n; i++) {
-  	  	printf("%d\n", input[i]); 
-  	}   
+           int num_threads = atoi(argv[3]);
+
+ 
+
+           if (n < 2 || num_threads < 1)
+
+                      exit(EXIT_FAILURE);
+
+ 
+
+           input = malloc(sizeof(int) * n);
+
+           read_input_vector(filename, n, input);
+           
+           //auxiliary array, flip between arrays on each d step, then you don't need conditional variable to check if previous chunk has been updated yet before summing (Horn 2005)
+          seq = (int*) malloc(sizeof(int) * n); 
+           
+          //pthread_t thread[num_threads];
+           
+          int* workl = (int*)malloc(sizeof(int));
+          *workl = n/num_threads;
+          
+          step_arg_t* args = (step_arg_t*)malloc(sizeof(step_arg_t) * num_threads);
+	  
+	  int d;
+          for(d = 0; d < log2(n); d += 1) //input when %2 == 0, seq when %2 == 1
+          {
+          	
+			step_arg_t* argN = &args[0];
+          		argN->end = *workl*0;//input index of end of chunk
+          		//printf("%d\n", argN->end);fflush(stdout);
+          		argN->beg = argN->end + *workl - 1;//input index of beginning of chunk
+          		//printf("%d\n", argN->beg);fflush(stdout);
+          		argN->step = d;
+          		//printf("%d\n", argN->step);fflush(stdout);
+         		
+          		pthread_t tid1;
+          		pthread_create(&tid1, NULL, step_sum, (void*)argN);
+          		
+			step_arg_t* arg2 = &args[1];
+          		arg2->end = *workl*1;//input index of end of chunk
+          		//printf("%d\n", argN->end);fflush(stdout);
+          		arg2->beg = arg2->end + *workl - 1;//input index of beginning of chunk
+          		//printf("%d\n", argN->beg);fflush(stdout);
+          		arg2->step = d;
+          		//printf("%d\n", argN->step);fflush(stdout);
+         		
+          		pthread_t tid2;
+          		pthread_create(&tid2, NULL, step_sum, (void*)arg2);          		
+          		
+           		pthread_join(tid1, NULL);         		
+          		pthread_join(tid2, NULL);
+          	//barrier, wait for all threads to complete
+          	
+          
+		  	//for(int k = n-1; k>0; k--) //index of input array, by starting at highest index, we avoid adding array value that has already been updated
+		  	//{
+			 // 	if(k >= (1u << d))
+			 // 	{
+			 // 		input[k] = input[(k) - (1u << d)] + input[k]; //critical section
+			//  	}
+			//} 
+          } 
+          
+         
+	if(d %2 == 0)
+	{
+           for (int i=0; i < n; i++) 
+           {
+
+                      printf("%d\n", input[i]);
+
+           } 
+       } 
+       else
+       {
+           for (int i=0; i < n; i++) 
+           {
+
+                      printf("%d\n", seq[i]);
+
+           }       
+       }
+
 }
-
-
