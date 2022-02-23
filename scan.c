fedf6ea472e7726d1e243cdf7a85dc55627dd9c2
Owen Radcliffe <62451536+radcliffeowe@users.noreply.github.com>
2022-02-23 14:20:21 -0500

    Scan.c with full parallelism and phased barrier

--- scan.c
+++ scan.c
@@ -24,7 +24,24 @@ sem_t sem;
 
 int workl;
 
-int d = 0;
+//int d = 0;
+
+int thrds_arrived = 0;
+
+int num_threads;
+
+int n;
+
+int log_size = 0;
+
+sem_t phase1;
+
+
+sem_t phase2;
+
+
+sem_t lock;
+
 
 void read_input_vector(const char* filename, int n, int* array)
 
@@ -80,7 +97,7 @@ typedef struct __step_arg_t {
 
            int end; // final operation
            
-           int step; //d
+           //int step; //d
 
 } step_arg_t;
 
@@ -92,40 +109,74 @@ void* step_sum(void* step_args)
 	
 	step_arg_t* arg = (step_arg_t*)step_args;
 	int k = arg->beg;
-	int d = arg->step;
-	//printf("%d %d\n", arg->beg, arg->step);fflush(stdout);
-	while(k>=arg->end) //index of input array, by starting at highest index, we avoid adding array value that has already been updated
+	int d;
+	//printf("%d %d %d\n", arg->beg, arg->end, n);fflush(stdout);
+	for(d = 0; d<log2(n); d+=1)
 	{
-		if(k >= (1u << d))
+		while(k>=arg->end) //index of input array, by starting at highest index, we avoid adding array value that has already been updated
 		{
-			if(d%2 == 0)
-				seq[k] = input[(k) - (1u << d)] + input[k]; //critical section
+			if(k >= (1u << d))
+			{
+				if(d%2 == 0)
+					seq[k] = input[(k) - (1u << d)] + input[k]; //critical section
+				else
+					input[k] = seq[(k) - (1u << d)] + seq[k];
+			}
 			else
-				input[k] = seq[(k) - (1u << d)] + seq[k];
-		}
-		else
+			{
+				if(d%2 == 0)
+					seq[k] = input[k]; //critical section
+				else
+					input[k] = seq[k];			
+			}
+			k--;
+		} 
+		k = arg->beg;
+		//barrier, ensure threads wait for every thread to finish step before moving to next one
+		sem_wait(&lock);
+		thrds_arrived += 1;
+		if(thrds_arrived == num_threads)
 		{
-			if(d%2 == 0)
-				seq[k] = input[k]; //critical section
-			else
-				input[k] = seq[k];			
+			sem_wait(&phase2);
+			sem_post(&phase1);
 		}
-		k--;
-	}  
+		sem_post(&lock);
+		
+		sem_wait(&phase1);
+		sem_post(&phase1);
+		
+		
+		
+		sem_wait(&lock);
+		thrds_arrived -= 1;
+		if(thrds_arrived == 0)
+		{
+			sem_wait(&phase1);
+			sem_post(&phase2);
+		}
+		sem_post(&lock);
+		
+		sem_wait(&phase2);
+		sem_post(&phase2);
+		//printf("%d\n", d);fflush(stdout);
+		
+	} 
+	log_size = d;
 	return 0;
 }
-
- 
 
 int main(int argc, char* argv[])
 
 {
-
+	sem_init(&phase1, 0, 0);
+	sem_init(&phase2, 0, 1);
+	sem_init(&lock, 0, 1);
+	
     char* filename = argv[1];
 
-           int n = atoi(argv[2]);
+           n = atoi(argv[2]);
 
-           int num_threads = atoi(argv[3]);
+           num_threads = atoi(argv[3]);
 
  
 
@@ -153,31 +204,26 @@ int main(int argc, char* argv[])
           
           step_arg_t** args = (step_arg_t**)malloc(sizeof(step_arg_t*) * num_threads);
           
-          //pthread_t tid1;
-          //pthread_t tid2;
-	  
-	  int d;
-          for(d = 0; d < log2(n); d += 1) //input when %2 == 0, seq when %2 == 1
+	  int* log2 = (int*)malloc(sizeof(int));
+          for(int i = 0; i<num_threads; i++)
           {
-          	for(int i = 0; i<num_threads; i++)
-          	{
-          		thread[i] = (pthread_t*)malloc(sizeof(pthread_t));
-          		//initialize thread ptrs
-          		args[i] = (step_arg_t*)malloc(sizeof(step_arg_t));
-			step_arg_t* argN = args[i];
-          		argN->end = *workl*i;//input index of end of chunk
-          		//printf("%d\n", argN->end);fflush(stdout);
-          		argN->beg = argN->end + *workl - 1;//input index of beginning of chunk
-          		//printf("%d\n", argN->beg);fflush(stdout);
-          		argN->step = d;
-          		//printf("%d\n", argN->step);fflush(stdout);
+          	thread[i] = (pthread_t*)malloc(sizeof(pthread_t));
+          	//initialize thread ptrs
+          	args[i] = (step_arg_t*)malloc(sizeof(step_arg_t));
+		step_arg_t* argN = args[i];
+        	argN->end = *workl*i;//input index of end of chunk
+        	//printf("%d\n", argN->end);fflush(stdout);
+        	argN->beg = argN->end + *workl - 1;//input index of beginning of chunk
+        	//printf("%d\n", argN->beg);fflush(stdout);
+        	//argN->step = d;
+        	//printf("%d\n", argN->step);fflush(stdout);
          		
-              		pthread_create(thread[i], NULL, step_sum, (void*)argN);          		
-          	}
-          	for(int i = 0;i<num_threads; i++)
-          	{
-          		pthread_join(*thread[i], NULL);
-          	}
+        	pthread_create(thread[i], NULL, step_sum, (void*)argN);          		
+        }
+        for(int i = 0;i<num_threads; i++)
+        {
+        	pthread_join(*thread[i], (void**)&log2);
+        }
 
           	//barrier, wait for all threads to complete
           	
@@ -188,11 +234,10 @@ int main(int argc, char* argv[])
 			 // 	{
 			 // 		input[k] = input[(k) - (1u << d)] + input[k]; //critical section
 			//  	}
-			//} 
-          } 
+			//}  
           
          
-	if(d %2 == 0)
+	if(log_size %2 == 0)
 	{
            for (int i=0; i < n; i++) 
            {
